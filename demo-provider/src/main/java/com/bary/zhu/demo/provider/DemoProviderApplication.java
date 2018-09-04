29aa9844a3e489bc8e27e73dfedecdc8823289ea
zhubin <zhubin@mapbar.com>
2018-09-04 11:32:43 +0800

    init

---
+++ demo-provider/src/main/java/com/bary/zhu/demo/provider/DemoProviderApplication.java
@@ -0,0 +1,12 @@
+package com.bary.zhu.demo.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProviderApplication.class, args);
+    }
+}
---
+++ demo-provider/src/test/java/com/bary/zhu/demo/provider/DemoProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bary.zhu.demo.provider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoProviderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
