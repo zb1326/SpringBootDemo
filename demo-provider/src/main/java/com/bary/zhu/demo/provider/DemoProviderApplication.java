94c900da5013916178caeb98fa379a8e4ea4e64d
zhubin <zhubin@mapbar.com>
2018-09-04 15:17:11 +0800

    init

--- demo-provider/src/main/java/com/bary/zhu/demo/provider/DemoProviderApplication.java
+++ demo-provider/src/main/java/com/bary/zhu/demo/provider/DemoProviderApplication.java
@@ -2,8 +2,10 @@ package com.bary.zhu.demo.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class DemoProviderApplication {
 
     public static void main(String[] args) {
